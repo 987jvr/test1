96e2bf70503f861f06e919b95becd6b7361fc18f
987jvr <98086412+987jvr@users.noreply.github.com>
2022-02-15 14:20:53 +0100

    jet

---
+++ TC/config/browser-versions.js
@@ -0,0 +1,22 @@
+﻿TC.Cfg.acceptedBrowserVersions = [
+    {
+        "name": "Chrome",
+        "version": 85
+    },
+    {
+        "name": "Edge",
+        "version": 85
+    },
+    {
+        "name": "Firefox",
+        "version": 78
+    },
+    {
+        "name": "Safari",
+        "version": 13
+    },
+    {
+        "name": "Mobile Safari",
+        "version": 13
+    }
+];
---
+++ TC/control/CacheBuilder.js
@@ -0,0 +1,7 @@
+﻿TC.control = TC.control || {};
+
+if (!TC.control.OfflineMapMaker) {
+    TC.syncLoadJS(TC.apiLocation + 'TC/control/OfflineMapMaker');
+}
+
+TC.control.CacheBuilder = TC.control.OfflineMapMaker;
